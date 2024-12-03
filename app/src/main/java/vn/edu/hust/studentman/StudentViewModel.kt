f1fdf1aa256be33ce325b3324467cc7469ba404c
xuanthanh112 <139263402+xuanthanh112@users.noreply.github.com>
2024-12-03 23:51:22 +0700

    Update StudentViewModel.kt

--- app/src/main/java/vn/edu/hust/studentman/StudentViewModel.kt
+++ app/src/main/java/vn/edu/hust/studentman/StudentViewModel.kt
@@ -6,26 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class StudentViewModel : ViewModel() {
     private val _students = MutableLiveData<MutableList<StudentModel>>(mutableListOf(
-//        StudentModel("Nguyễn Văn An", "SV001"),
-//        StudentModel("Trần Thị Bảo", "SV002"),
-//        StudentModel("Lê Hoàng Cường", "SV003"),
-//        StudentModel("Phạm Thị Dung", "SV004"),
-//        StudentModel("Đỗ Minh Đức", "SV005"),
-//        StudentModel("Vũ Thị Hoa", "SV006"),
-//        StudentModel("Hoàng Văn Hải", "SV007"),
-//        StudentModel("Bùi Thị Hạnh", "SV008"),
-//        StudentModel("Đinh Văn Hùng", "SV009"),
-//        StudentModel("Nguyễn Thị Linh", "SV010"),
-//        StudentModel("Phạm Văn Long", "SV011"),
-//        StudentModel("Trần Thị Mai", "SV012"),
-//        StudentModel("Lê Thị Ngọc", "SV013"),
-//        StudentModel("Vũ Văn Nam", "SV014"),
-//        StudentModel("Hoàng Thị Phương", "SV015"),
-//        StudentModel("Đỗ Văn Quân", "SV016"),
-//        StudentModel("Nguyễn Thị Thu", "SV017"),
-//        StudentModel("Trần Văn Tài", "SV018"),
-//        StudentModel("Phạm Thị Tuyết", "SV019"),
-//        StudentModel("Lê Văn Vũ", "SV020")
+
         StudentModel("Nguyễn Thị Anh", "20220001"),
         StudentModel("Trần Văn Bình", "20220002"),
         StudentModel("Lê Thị Cẩm", "20220003"),
